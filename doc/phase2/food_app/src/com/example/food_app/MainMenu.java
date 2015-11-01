9cc43aeae1158179d5702759346f5506eafb76be
dbchung2 <dbchung2@gmail.com>
2015-11-01 08:56:44 +0800

    main menu

--- doc/phase2/food_app/src/com/example/food_app/MainMenu.java
+++ doc/phase2/food_app/src/com/example/food_app/MainMenu.java
@@ -1,10 +1,14 @@
 package com.example.food_app;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
 
@@ -12,6 +16,7 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		next_action();
 	}
 
 	@Override
@@ -24,5 +29,25 @@ public class MainMenu extends Activity {
 	public void goToWishlist(View view) {
 		Intent intent = new Intent(this, Wishlist.class);
 		startActivity(intent);
+	}
+	
+	public void next_action() {
+		Button Whishlists;
+		Button Restaurants;
+		Button dishes;
+		
+		Whishlists = (Button) findViewById(R.id.Whishlists);
+		Restaurants = (Button) findViewById(R.id.Restaurants);
+		dishes = (Button) findViewById(R.id.dishes);
+
+		Whishlists.setOnClickListener(new OnClickListener() {
+			
+		
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View arg0) {
+				setContentView(R.layout.activity_wishlist);
+			}					
+		});
 	}
 }
