c5eb532493399011ecec6f72114b9233da2669be
dbchung2 <dbchung2@gmail.com>
2015-11-02 07:34:24 +0800

    restuarant implemented

--- doc/phase2/food_app/src/com/example/food_app/Restaurant.java
+++ doc/phase2/food_app/src/com/example/food_app/Restaurant.java
@@ -1,15 +1,27 @@
 package com.example.food_app;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Restaurant extends Activity {
+	ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_restaurant);
+		listView = (ListView)findViewById(R.id.listView_restuarant1);
+        //Insert Array here
+		String[] a = {"pizza","coke"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
+        listView.setAdapter(adapter);
 	}
 
 	@Override
@@ -19,4 +31,9 @@ public class Restaurant extends Activity {
 		return true;
 	}
 
+	public void searchRestaurant(View view) {
+		//Method to search the restaurant list
+	}
 }
+
+
