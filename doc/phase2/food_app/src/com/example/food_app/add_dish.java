badd5e0dcd27cfc598505d7e959234b9f3c1cba5
Matt <matt.mcwaters@mail.utoronto.ca>
2015-11-01 21:07:56 -0500

    added add_dish activity

---
+++ doc/phase2/food_app/src/com/example/food_app/add_dish.java
@@ -0,0 +1,14 @@
+package com.example.food_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Matt on 2015-11-01.
+ */
+public class add_dish extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dish);
+    }
+}
