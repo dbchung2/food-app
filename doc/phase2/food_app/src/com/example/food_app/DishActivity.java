aac5bbbebe2b8c525458067c50b7ca044a29e904
dbchung2 <dbchung2@gmail.com>
2015-11-02 10:00:24 +0800

    dish activity updated

--- doc/phase2/food_app/src/com/example/food_app/DishActivity.java
+++ doc/phase2/food_app/src/com/example/food_app/DishActivity.java
@@ -1,14 +1,55 @@
 package com.example.food_app;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by Matt on 2015-11-01.
- */
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.food_app.DatabaseClasses.MySQLiteHelper;
+
 public class DishActivity extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.all_dishes_activity);
-    }
+	ListView listView;
+		MySQLiteHelper db = new MySQLiteHelper(this);
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.activity_restaurant);
+				listView = (ListView)findViewById(R.id.listView_dish1);
+				//Insert Array here
+				ArrayList<String> restaurantInfo = new ArrayList<String>();
+				ArrayList<com.example.food_app.DatabaseClasses.Restaurant> allRestaurants = db.getAllRestaurants();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, restaurantInfo);
+        listView.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.restaurant, menu);
+		return true;
+	}
+
+	//public void searchRestuarant(View view) {
+		//Method to search the restaurant list
+	//	String[] restuarant = {"pizza","coke"}; // get the restuarant list form the database
+		//EditText name = (EditText)findViewById(R.id.restuarant_name);
+		//String string_name = name.toString();
+		//for (int i=0; i< restuarant.length; i++) {
+		//    if (restuarant[i].startsWith(string_name)){
+		//    	
+		//    	}
+		//    }
+		    	
+	//	}
 }
+
+
